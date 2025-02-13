5ada3d52116a0e76380f9d5d08cd0ccf52029d90
meanoldgit <alexantonaranda@gmail.com>
2025-02-13 20:05:54 +0100

    range calendar implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,36 @@
 "use client";
 import * as React from 'react';
-import { CNavbar } from "@/components/CNavbar";
-import { Box, Container } from "@mui/material";
-import { LocalizationProvider } from "@mui/x-date-pickers";
+// import { Box, Container } from "@mui/material";
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { StaticDateRangePicker } from "@mui/lab";
+import { Box, TextField } from '@mui/material';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+
+
 
 export default function Home() {
+  const[value, setValue] = React.useState([null, null])
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateCalendar />
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <StaticDateRangePicker
+        displayStaticWrapperAs="desktop"
+        key={1}
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+        renderInput={(startProps, endProps) => (
+          <React.Fragment>
+            <TextField {...startProps} />
+            <Box sx={{ mx: 2 }}> to </Box>
+            <TextField {...endProps} />
+          </React.Fragment>
+        )}
+      >
+      </StaticDateRangePicker>
+    <p>{value[0]?.toString()}</p>
     </LocalizationProvider>
   );
 }
