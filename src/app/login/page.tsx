5179445fbefbd17767a96833e8ff112e6728bd7c
meanoldgit <alexantonaranda@gmail.com>
2025-02-18 21:32:48 +0100

    removed unused variable for deployment

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 import { Box } from "@mui/material";
-import { useState } from "react";
 
-export default function Event() {
-  const [value, setValue] = useState<[Date | null, Date | null]>([null, null]);
-
+export default function Login() {
   return (
     <Box>
     </Box>
