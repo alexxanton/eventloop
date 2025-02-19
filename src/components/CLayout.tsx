9b314ad68d3b452e349d10f2dca6c549d8e5d359
meanoldgit <alexantonaranda@gmail.com>
2025-02-20 00:28:10 +0100

    deploy blank page fix attempt

--- src/components/CLayout.tsx
+++ src/components/CLayout.tsx
@@ -39,7 +39,7 @@ export function CLayout({
     
   });
 
-  if (theme === "") return null;
+  // if (theme === "") return null;
 
   return (
     <ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
