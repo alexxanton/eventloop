3d0b0dda4903f5f16b849e2b1f3ff367ee8773a8
meanoldgit <alexantonaranda@gmail.com>
2025-04-09 17:01:27 +0200

    updated dynamic page props

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,15 +1,12 @@
 import { CEvent } from "@/components/events/CEvent";
 import { supabase } from "@/utils/supabase";
 
-interface PageProps {
-  params: { id: string };
-}
-
-export default async function Page({params}: PageProps) {
+export default async function Page({params}: {params: Promise<{ id: string }>}) {
+  const { id } = await params;
   const {data: event} = await supabase
     .from("events")
     .select("*")
-    .eq("id", params.id)
+    .eq("id", id)
     .single();
   
   return <CEvent event={event} />;
