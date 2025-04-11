d25df6e8bc2399b7a4747534739fbb7202dea328
meanoldgit <alexantonaranda@gmail.com>
2025-04-11 14:01:30 +0200

    improved group fetching query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,22 @@
 import * as React from 'react';
 import { CGroupView } from '@/components/groups/chat/CGroupView';
-// import { supabase } from '@/utils/supabase/supabase';
-import { cookies } from "next/headers";
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
-  // const userId = await createClient();
-  // console.log(userId.auth.getUser())
-
   const supabase = await createClient()
   const { data: { user }, error } = await supabase.auth.getUser()
+  
   if (error || !user) {
-    redirect('/login')
+    redirect("/login")
   }
   
   const { data: groups } = await supabase
     .from("groups")
-    .select("*")
-    .in("id", 
-        ((await supabase
-            .from("group_members")
-            .select("group_id")
-            .eq("user_id", user.id))
-            .data ?? []).map(member => member.group_id)
-    );
+    .select("*, group_members!inner(*)")
+    .eq("group_members.user_id", user.id)
 
-    console.log(groups)
+  console.log(groups)
 
   return <CGroupView groups={groups} />;
 }
