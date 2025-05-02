c49d162633bfb2726bfdc18750203db9522ab308
meanoldgit <alexantonaranda@gmail.com>
2025-05-02 03:37:34 +0200

    made the tickets beautiful and added a search bar

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,35 +1,7 @@
-import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
 import { CTickets } from "@/components/events/CTickets";
+import { supabase } from "@/utils/supabase";
 
 export default async function Page() {
-  const cookieStore = cookies();
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    // process.env.SUPABASE_SERVICE_ROLE_KEY!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        async get(name) {
-          return (await cookieStore).get(name)?.value;
-        },
-        set(name, value, options) {
-        },
-        remove(name, options) {
-        },
-      },
-    }
-  );
-
-  const { data: { user } } = await supabase.auth.getUser();
-  console.log(user);
-  
-
-  // if (!user) {
-  //   return <div>You need to be logged in.</div>;
-  // }
-
   const { data } = await supabase
     .from("tickets")
     .select("*, events!inner(*)")
