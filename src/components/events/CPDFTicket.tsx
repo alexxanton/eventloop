beac427c96cf0f0fbcd22b69fd63200b2bb721fd
Judith <julove@prateducacio.com>
2025-05-15 17:23:39 +0200

    export pdf content

--- src/components/events/CPDFTicket.tsx
+++ src/components/events/CPDFTicket.tsx
@@ -1,3 +1,25 @@
 import { TicketType } from "@/utils/types/types";
 import { Box, Theme, Typography } from "@mui/material";
 import QRCode from "react-qr-code";
+
+export const CPDFTicket = ({
+  ticket,
+  theme,
+}: {
+  ticket: TicketType;
+  theme: Theme;
+}) => (
+  <Box
+    id="pdf-content"
+    sx={{
+      width: "100%",
+      height: "100%",
+      backgroundColor: "#f4f4f9", // soft background
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      padding: "40px",
+      boxSizing: "border-box",
+    }}
+  >
+    
